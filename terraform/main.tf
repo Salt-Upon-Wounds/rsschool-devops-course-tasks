8b39ad1288c8784c3280457d0db67822b50105dd
Kirill <kiw197204@gmail.com>
2025-06-12 23:01:39 +0400

    fix: terraform files format

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-  enable_dns_support = true
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
@@ -17,10 +17,10 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_subnet" "public" {
-  count             = length(var.public_subnet_cidrs)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_cidrs[count.index]
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
 
   tags = {
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,7 +6,7 @@ terraform {
   required_version = ">= 1.3.0"
 
   backend "s3" {
-    bucket = "rsschool-devops-terraform-state-kirylkatselnikau"  # заменишь позже на свой
+    bucket = "rsschool-devops-terraform-state-kirylkatselnikau" # заменишь позже на свой
     key    = "networking/terraform.tfstate"
     region = "us-east-1"
   }
